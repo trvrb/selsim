5dcb61ccf9b6525d76082b8251853e1a6c5421b7
Trevor Bedford <bedfordt@umich.edu>
2009-07-12 00:08:08 -0400

    Files included.

---
+++ sample.cpp
@@ -0,0 +1,24 @@
+/* sample.cpp
+Member function definitions for Sample class
+*/
+
+#include "sample.h"
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include <cstdlib>
+#include <map>
+#include <set>
+#include <vector>
+#include <cmath>
+#include <sstream>
+
+/* Constructor function to initialize private data */
+Sample::Sample() {
+	
+
+	
+}
+
---
+++ sample.h
@@ -0,0 +1,25 @@
+/* sample.h
+Sample class definition
+This object contains a set of sequence objects marked with their date of collection.
+*/
+
+#ifndef SAMPLE_H
+#define SAMPLE_H
+
+using namespace std;
+
+#include <map>
+#include <set>
+#include <vector>
+
+class Sample {
+
+public:
+	Sample();								// constructor
+																		
+private:
+
+	
+};
+
+#endif
